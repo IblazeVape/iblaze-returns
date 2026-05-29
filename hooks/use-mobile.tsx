3fc14861f01b8c2f0275568a32d59d35ebd04d22
IblazeVape <vapeumbrellamanchester@gmail.com>
2026-05-29 13:56:01 +0100

    Update use-mobile.tsx

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -6,13 +6,22 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const widthMql  = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const orientMql = window.matchMedia("(orientation: landscape)")
+
+    const update = () => {
+      // In landscape, use the desktop icon-rail sidebar regardless of width
+      setIsMobile(!orientMql.matches && window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    widthMql.addEventListener("change", update)
+    orientMql.addEventListener("change", update)
+    update()
+
+    return () => {
+      widthMql.removeEventListener("change", update)
+      orientMql.removeEventListener("change", update)
+    }
   }, [])
 
   return !!isMobile
